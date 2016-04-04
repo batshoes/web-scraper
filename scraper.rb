3dd05dd3a39c4337214d79b1485e7588cab85fab
James Middlemiss <middlemissj.usa@gmail.com>
2016-04-04 11:33:31 -0400

    scrapes for all 208 pages

--- scraper.rb
+++ scraper.rb
@@ -8,7 +8,7 @@ ADDRESS = "http://www.usacreditunions.com/"
 results = []
 
 
-for i in 1..2 do
+for i in 1..208 do
   base_url = "http://www.usacreditunions.com/usa-credit-unions-deposit-rates-page#{i}"
 
   scraper.get(base_url) do |search|
@@ -37,16 +37,18 @@ results.each do |site|
     data_search.css('.infoTables').css('.row-detail')[0..27].each do |val|
       result_array << val.text
     end
-    
+
     results_hash = Hash[*result_array]
+    
     results_hash.keys.each do |key|
       site[key] = results_hash[key]
     end
 
   end
+  puts site[:name] + " done..."
 end
 
-CSV.open("new.csv", "wb") do |csv|
+CSV.open("usa-credit-unions.csv", "wb") do |csv|
   csv << results.first.keys # adds the attributes name on the first line
   results.each do |hash|
     csv << hash.values
